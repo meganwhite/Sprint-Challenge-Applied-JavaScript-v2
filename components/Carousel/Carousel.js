593e7d0a40d505f071130f4f0117f0a141c620af
meganwhite <mfw247@gmail.com>
2019-07-13 02:33:52 +0900

    write function to create carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,37 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+
+function createCarousel() {
+  // create elements
+  const carousel = document.createElement('div');
+  const leftButton = document.createElement('div');
+  const img1 = document.createElement('img');
+  const img2 = document.createElement('img');
+  const img3 = document.createElement('img');
+  const img4 = document.createElement('img');
+  const rightButton = document.createElement('div');
+
+  // set classes
+  carousel.classList.add('carousel');
+  leftButton.classList.add('left-button');
+  rightButton.classList.add('right-button');
+
+  // create structure
+  carousel.appendChild(leftButton);
+  carousel.appendChild(img1);
+  carousel.appendChild(img2);
+  carousel.appendChild(img3);
+  carousel.appendChild(img4);
+  carousel.appendChild(rightButton);
+
+  // set content
+  img1.setAttribute('src','./assets/carousel/mountains.jpeg');
+  img2.setAttribute('src','./assets/carousel/computer.jpeg');
+  img3.setAttribute('src','./assets/carousel/trees.jpeg');
+  img4.setAttribute('src','./assets/carousel/turntable.jpeg');
+
+  return carousel;
+
+}
